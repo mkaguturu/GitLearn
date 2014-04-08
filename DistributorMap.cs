eb0e8fd0f79271cea93fcd74774c6340518947a7
mkaguturu <mkaguturu@gmail.com>
2014-04-08 21:26:04 +0530

    new start all over again

---
+++ DistributorMap.cs
@@ -0,0 +1,37 @@
+﻿using Nop.Core;
+using SDK.IT.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SDK.IT.Data.Domain.Business
+{
+    public class DistributorMap : EntityTypeConfiguration<Distributor>
+    {
+        public DistributorMap()
+        {
+            const string DESCRIMINATOR = "Changed the Descriminator";
+
+            ToTable("IT_Distributor");
+            HasKey(e => e.Id);
+
+            Map<Entrepreneur>(m => m.Requires(DESCRIMINATOR).HasValue((int)DistributorType.Entrepreneur));
+            Map<ITCustomer>(m => m.Requires(DESCRIMINATOR).HasValue((int)DistributorType.Customer));
+
+            HasOptional(d => d.PersonalDetails).WithRequired();
+            HasOptional(d => d.WebAdministration).WithRequired();
+            HasOptional(d => d.ActivityLog).WithRequired();
+            HasOptional(d => d.BankDetails).WithRequired();
+            HasOptional(d => d.D_TaxDetails).WithRequired();
+
+            HasRequired(d => d.SourceType).WithMany().HasForeignKey(d => d.SourceTypeId);
+            HasRequired(d => d.BusinessType).WithMany().HasForeignKey(d => d.BusinessTypeId);
+            HasRequired(d => d.PreferredLanguageType).WithMany().HasForeignKey(d => d.PreferredLanguageTypeId);
+
+            HasOptional(d => d.Sponsor).WithMany(p => p.SponsoredList).HasForeignKey(d => d.SponsorId).WillCascadeOnDelete(false);
+            HasOptional(d => d.Upline).WithMany(p => p.DownlineList).HasForeignKey(d => d.UplineId).WillCascadeOnDelete(false);
+
+            HasMany(d => d.LogDetails).WithRequired(log => log.DistributorEntity).HasForeignKey(l => l.DistributorId).WillCascadeOnDelete(false);
+        }
+    }
+}
---
+++ DistributorModel.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages.Html;
+using Nop.Services.Directory;
+using Nop.Services.Localization;
+using SDK.ImmunoTech.Models.People;
+using SDK.IT.Core.Domain.Business;
+using SDK.IT.Services.Business;
+using SDK.IT.Services.People;
+
+namespace SDK.ImmunoTech.Models.Business
+{
+    public class TypeListModel
+    {
+        public int TypeId { get; set; }
+
+        public String Name { get; set; }
+    }
+
+    public class DistributorModel
+    {
+        private ILanguageService _languageService;
+        private ICountryService _countryservice;
+        private IStateProvinceService _stateProvinceService;
+        private IDistributorService _distributorService;
+        private IPersonalDetailsService _personalDetailsService;
+
+        public DistributorModel()
+        {
+           
+        }
+
+        //public DistributorModel() { }
+
+        public static implicit operator Distributor(DistributorModel model)
+        {
+            Distributor d = null;
+            if(model.IsEntrepreneur)
+            {
+                d = new Entrepreneur();
+            }
+            else
+            {
+                d = new ITCustomer();
+            }
+
+            d.Id = model.DistributorId;
+
+            if(model.SponsorId > 0)
+            {
+                d.SponsorId = model.SponsorId;
+            }
+
+            if(model.UplineId > 0)
+            {
+                d.UplineId = model.UplineId;
+            }
+
+            d.DistributorCode = model.DistributorCode;
+            d.IsActive = true;
+
+            d.SourceTypeId = model.InformationSourceTypeId;
+            d.BusinessTypeId = model.BusinessTypeId;
+            d.PreferredLanguageTypeId = model.LanguageTypeId;
+
+            /*
+             * Implicit conversions enabled
+             * 
+             * makes code more readable
+             * even though we have to jump to different files to debug etc.
+             */
+            d.PersonalDetails = model.PersonalDetails;
+            d.ContactRef = model.ContactRef;
+
+            d.D_TaxDetails = model.D_TaxDetails;
+            d.WebAdministration = model.WebAdministration;
+            d.ActivityLog = model.ActivityLog;
+            d.BankDetails = model.BankDetails;
+
+            return d;
+        }
+
+        public void InitAllLists(ILanguageService languageService,
+                        ICountryService countryservice,
+                        IStateProvinceService stateProvinceService,
+                        IPersonalDetailsService personalDetailsService,
+                        IDistributorService distributorService)
+        {
+            _languageService = languageService;
+            _countryservice = countryservice;
+            _stateProvinceService = stateProvinceService;
+            _personalDetailsService = personalDetailsService;
+            _distributorService = distributorService;
+            
+            LanguagesList = from lang in _languageService.GetAllLanguages()
+                            select new TypeListModel() { TypeId = lang.Id, Name = lang.Name };
+
+            CountryList = from country in _countryservice.GetAllCountries()
+                          select new TypeListModel() { TypeId = country.Id, Name = country.Name};
+
+            GenderList = from gender in _personalDetailsService.GetGenderTypeList()
+                            select new TypeListModel() { TypeId = gender.Id, Name = gender.GenderTypeCode};
+
+            InformationSourceList = from info in _distributorService.GetDistributorInformationSourceTypeList()
+                                    select new TypeListModel() { TypeId = info.Id, Name = info.InformationCode };
+
+            BusinessTypeList = from business in _distributorService.GetDistributorBusinessTypeList() 
+                               select new TypeListModel() { TypeId = business.Id, Name = business.BusinessTypeCode };
+
+            PayTypeList = from pay in _distributorService.GetDistributorPayTypeList()
+                          select new TypeListModel() { TypeId = pay.Id, Name = pay.PayTypeCode};
+
+            GeneratedDistributorCode = _distributorService.GenerateDistributorCode();
+        }
+
+        public int DistributorId { get; set; }
+        
+        public int SponsorId { get; set; }
+
+        public string SponsorName { get; set; }
+
+        public int UplineId { get; set; }
+
+        public string UplineName { get; set; }
+
+        public int DistributorCode { get; set; }
+
+        public int GeneratedDistributorCode { get; set; }
+
+        public bool IsEntrepreneur { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int InformationSourceTypeId { get; set; }
+
+        public int LanguageTypeId { get; set; }
+
+        public int BusinessTypeId { get; set; }
+        
+        public int PayTypeId { get; set; }
+
+        //public int GenderTypeId { get; set; }
+
+        public IEnumerable<TypeListModel> LanguagesList { get; private set; }
+               
+        public IEnumerable<TypeListModel> CountryList { get; private set; }
+               
+        public IEnumerable<TypeListModel> StateProvinceList { get; private set; }
+
+        public IEnumerable<TypeListModel> GenderList { get; private set; }
+
+        public IEnumerable<TypeListModel> InformationSourceList { get; set; }
+               
+        public IEnumerable<TypeListModel> BusinessTypeList { get; private set; }
+                
+        public IEnumerable<TypeListModel> PayTypeList { get; set; }
+
+        public ContactModel ContactRef { get; set; }
+
+        public PersonModel PersonalDetails { get; set; }
+
+        public DistributorTaxDetailsModel D_TaxDetails { get; set; }
+        
+        public DistributorWebAdministrationModel WebAdministration { get; set; }
+
+        public DistributorActivityLogModel ActivityLog { get; set; }
+
+        public DistributorBankDetailsModel BankDetails { get; set; }
+
+
+        //public string NoteBook { get; set; }
+
+        //public DistributorModel Sponsor { get; set; }
+
+        //public DistributorModel Upline { get; set; }
+
+        //public ICollection<Distributor> DownlineList { get; set; }
+
+        //public ICollection<Distributor> SponsoredList { get; set; }
+
+        //public virtual ICollection<DistributorEntityLog> LogDetails { get; set; }       
+    }
+}
