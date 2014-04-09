092950e20b38b1f648a06d5271e42e6a00c3aec8
Pavan <pavan1@quadone.com>
2014-04-09 18:43:38 +0530

    conflict from master

--- DistributorModel.cs
+++ DistributorModel.cs
@@ -12,7 +12,7 @@ using SDK.IT.Services.People;
 
 namespace SDK.ImmunoTech.Models.Business
 {
-    public class TypeListModelForConflict
+    public class TypeListModelFromMaster
     {
         public int TypeId { get; set; }
 
